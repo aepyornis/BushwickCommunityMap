993078e44b444d14b492c188a69403106187909c
Chris Henrick <chrishenrick@gmail.com>
2014-11-24 15:25:43 -0500

    tweak dob permit tool tip style and data

--- map/js/main.js
+++ map/js/main.js
@@ -4,7 +4,7 @@ var app = app || {};
 // keep our map stuff in a part of the app object as to not pollute the global name space
 app.map = (function(w,d, $, _){
 
-  /*** local variables for map parts and layers ***/
+  // local variables for map parts and layers 
   var el = {
     map : null,
     cdbURL : null,
@@ -13,6 +13,7 @@ app.map = (function(w,d, $, _){
     tonerLite : null,
     satellite : null,
     taxLots : null,
+    baseLayers : null,
     dobPermits : null,
     dobPermitsA1 : null,
     dobPermitsA2A3 : null,
@@ -34,11 +35,12 @@ app.map = (function(w,d, $, _){
     vacant : "SELECT * FROM bushwick_pluto14v1 WHERE landuse = '11'",
   };
 
+  // use google maps api geocoder
   el.geocoder = new google.maps.Geocoder();
                                                                            
-  // set up the map
+  // set up the map!
   var initMap = function() {
-    // map paramaters
+    // map paramaters to pass to Leaflet
     var params = {
       center : [40.6941, -73.9162],
       minZoom : 14,
@@ -48,25 +50,27 @@ app.map = (function(w,d, $, _){
       zoomControl : false
     }
 
-    // instantiate the Leaflet map
+    // instantiate the Leaflet map object
     el.map = L.map('map', params);
-    // stamen toner lite base layer
-     el.tonerLite = new L.StamenTileLayer('toner-lite');
+    // add stamen toner lite base layer
+    el.tonerLite = new L.StamenTileLayer('toner-lite');
     el.map.addLayer(el.tonerLite);    
 
     // add Bing satelitte imagery layer
     el.satellite = new L.BingLayer('AkuX5_O7AVBpUN7ujcWGCf4uovayfogcNVYhWKjbz2Foggzu8cYBxk6e7wfQyBQW');
 
-
+    // object to pass Leaflet Control
      el.baseLayers = {
         streets: el.tonerLite,
         satellite: el.satellite
     };
 
+    // inits UI element for toggling base tile layers
     L.control.layers(el.baseLayers, {}, {
           position: 'bottomleft'
       }).addTo(el.map);
 
+    // makes sure base layers stay below the cartodb data
     el.map.on('baselayerchange', function(e){
       e.layer.bringToBack();
     })   
@@ -75,8 +79,7 @@ app.map = (function(w,d, $, _){
     getCDBData(params);
   } 
 
-  // function to load map pluto tax lot layer and dob permit layer 
-  // from CartoDB 
+  // function to load map pluto tax lot layer and dob permit layer from CartoDB
   var getCDBData = function(mapOptions) {  
     cartodb.createLayer(el.map, el.cdbURL, {
         cartodb_logo: false, 
@@ -88,73 +91,76 @@ app.map = (function(w,d, $, _){
         layer.getSubLayer(0).setSQL(el.sql.all);
         el.taxLots = layer.getSubLayer(0);
 
-        // store the dob permits a1 sublayer
+        // create and store the dob permits a1 sublayer
         el.dobPermitsA1 = layer.createSubLayer({
           sql : "SELECT * FROM exp_codedjobs_a1",
-          cartocss : '#exp_codedjobs_a1 {marker-width: 7; marker-fill: hsl(0,0%,35%); marker-line-color: white; marker-line-width: 0;}',
-          interactivity: ['address']
+          cartocss : '#exp_codedjobs_a1 {marker-width: 10; marker-fill: hsl(0,0%,30%); marker-line-color: white; marker-line-width: 0.8;}'
         });
 
-        // store the dob permits a2a3 sublayer
+        // create and store the dob permits a2a3 sublayer
         el.dobPermitsA2A3 = layer.createSubLayer({
           sql : "SELECT * FROM exp_codedjobs_a2a3",
-          cartocss : '#exp_codedjobs_a1 {marker-width: 7; marker-fill: hsl(100,0%,60%); marker-line-color: white; marker-line-width: 0;}',
-          interactivity: 'address'
+          cartocss : '#exp_codedjobs_a1 {marker-width: 10; marker-fill: hsl(100,0%,50%); marker-line-color: white; marker-line-width: 0.8;}'
         });
 
-        // store the dob permits nb sublayer
+        // create and store the dob permits nb sublayer
         el.dobPermitsNB = layer.createSubLayer({
           sql : "SELECT * FROM exp_codedjobs_nb",
-          cartocss : '#exp_codedjobs_a1 {marker-width: 7; marker-fill: hsl(350,0%,10%); marker-line-color: white; marker-line-width: 0;}',
-          interactivity: 'address'
+          cartocss : '#exp_codedjobs_a1 {marker-width: 10; marker-fill: hsl(350,0%,0%); marker-line-color: white; marker-line-width: 0.8;}'         
         });
 
+        // positions the tool tip in relationship to user's mouse
+        // offset it by 5px vertically and horizontally so the mouse arrow won't cover it
         var event = function(e){
               $('#tool-tip').css({
-                 left:  e.pageX,
-                 top:   e.pageY
+                 left:  e.pageX + 5,
+                 top:   e.pageY + 5
               });
-          };
+          };                                
 
-        el.dobPermitsA1.infowindow.set({
-          'template' : $('#infowindow_template').html()
-        })
-          .on('error', function(err){
-            console.log('infowindow error: ', err);
-          });                                 
-
-        // hide sublayers for dob permits
+        // set interactivity on the DOB permit layers
         var num_sublayers = layer.getSubLayerCount();
         for (var i = 1; i < num_sublayers; i++) { 
-          //console.log('sublayers: ', layer.getSubLayer(i)); 
-          layer.getSubLayer(i).setInteraction(true);          
-          // layer.getSubLayer(i).infowindow.set({
-          //   'template' : $('#infowindow_template').html()
-          // })
-          //   .on('error', function(err){
-          //     console.log('infowindow error: ', err);
-          //   });
+          // turn on interactivity for mousing events
+          layer.getSubLayer(i).setInteraction(true);
+          // tell cdb what columns to pass
+          layer.getSubLayer(i).setInteractivity('address, jt_description, ownername, ownerphone, ownerbusin, existingst, proposedst');          
           
-          layer.getSubLayer(i).on('featureOver', function(e,pos,latlng,data){
-            $('#tool-tip').html('<p>'  + data.address + '</p>');
+          // when the user mouses over the dob permit display html & data in a tool tip
+          layer.getSubLayer(i).on('featureOver', function(e, pos, latlng, data) {
+            $('#tool-tip').html(
+                                // text to display when user hovers on dob permit layers
+                                '<h4>DOB Permit Info</h4>' +
+                                '<hr>' +
+                                '<p><strong>Address:</strong> '  + data.address + '</p>' +
+                                '<p><strong>Job Description:</strong> ' + data.jt_description + '</p>' +
+                                '<p><strong>Owner Name:</strong> '  + data.ownername + '</p>' +
+                                '<p><strong>Owner Business:</strong> '  + data.ownerbusin + '</p>' +
+                                '<p><strong>Owner Phone:</strong> '  + data.ownerphone + '</p>' +
+                                '<p><strong>Existing Building Stories:</strong> '  + data.existingst + '</p>' +
+                                '<p><strong>Proposed Building Stories:</strong> '  + data.proposedst + '</p>'
+                                );
             $(document).bind('mousemove', event);
             $('#tool-tip').show();
           });
-
-          layer.getSubLayer(i).on('featureOut', function(e,pos,latlng,data){
-           //Make the tooltip go away when off cities
+          
+          // when the user mouses out remove the tool tip
+          layer.getSubLayer(i).on('featureOut', function(e,pos,latlng,data){           
             $('#tool-tip').hide();
             $(document).unbind('mousemove', event, false);
           });
 
+          // hide the dob permit layers when map loads
           layer.getSubLayer(i).hide();
 
-        }// end for loop
+        } // end sublayer for loop
 
+      // add the cdb layer to the map
       el.map.addLayer(layer, false);
+      // make sure the base layer stays below the cdb layer
       el.tonerLite.bringToBack();
 
-      });//.addTo(el.map);
+      }); // end cartodb.createLayer
 
   };
 
@@ -216,9 +222,7 @@ app.map = (function(w,d, $, _){
     // toggle A1 major alterations layer
     $a1.change(function(){
       if ($a1.is(':checked')){
-        el.dobPermitsA1.show();
-        // el.dobPermitsA1.setInteraction(true);
-        el.dobPermitsA1.setInteractivity('address');
+        el.dobPermitsA1.show();      
       } else {
         el.dobPermitsA1.hide();
       };
@@ -227,9 +231,7 @@ app.map = (function(w,d, $, _){
     // toggle A2, A3 minor alterations layer
     $a2a3.change(function(){
       if ($a2a3.is(':checked')){
-        el.dobPermitsA2A3.show();
-        // el.dobPermitsA2A3.setInteraction(true);
-        //el.dobPermitsA2A3.setInteractivity('address, job, initialcos, jt_description, ownerphone, ownername, ownertype');
+        el.dobPermitsA2A3.show();        
       } else {
         el.dobPermitsA2A3.hide();
       };
@@ -238,9 +240,7 @@ app.map = (function(w,d, $, _){
     // toggle NB new buildings layer
     $nb.change(function(){
       if ($nb.is(':checked')){
-        el.dobPermitsNB.show();
-        // el.dobPermitsNB.setInteraction(true);
-        // el.dobPermitsNB.setInteractivity('address, job, initialcos, jt_description, ownerphone, ownername, ownertype'); 
+        el.dobPermitsNB.show();        
       } else {
         el.dobPermitsNB.hide();
       };
@@ -249,17 +249,15 @@ app.map = (function(w,d, $, _){
 
   // geocode search box text and create a marker on the map
   var geocode = function(address) {
-    // bounding box for nyc to improve geocoder results
-    // 40.678685,-73.942451,40.710247,-73.890266
+    // reference bounding box for Bushwick to improve geocoder results: 40.678685,-73.942451,40.710247,-73.890266
     var bounds = new google.maps.LatLngBounds(
-          new google.maps.LatLng(40.678685,-73.942451), //sw
-          new google.maps.LatLng(40.710247,-73.890266) //ne
+          new google.maps.LatLng(40.678685,-73.942451), // sw
+          new google.maps.LatLng(40.710247,-73.890266) // ne
           );    
       el.geocoder.geocode({ 'address': address, 'bounds' : bounds }, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
           var latlng = [results[0].geometry.location.k , results[0].geometry.location.B];
-          console.log('gecoder results: ', results);
-          // console.log('latlng: ', latlng);
+          // console.log('gecoder results: ', results);
           
           // remove geocoded marker if one already exists
           if (el.geocoderMarker) { 
@@ -273,7 +271,7 @@ app.map = (function(w,d, $, _){
       });
   }
 
-  // search box interaction
+  // search box ui interaction
   var searchAddress = function() {
     $('#search-box').focus(function(){
       if ($(this).val()==="Search for a Bushwick address") {

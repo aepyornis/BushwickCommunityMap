870a7d31016bce7e03e04ed9dffa201322e559b6
gianordoli <gianordoligabriel@yahoo.com.br>
2014-12-07 20:05:31 -0500

    Finished intro+slides css

--- map/js/slide_toggle.js
+++ map/js/slide_toggle.js
@@ -1,6 +1,20 @@
-$('#toggle_slides').bind('mouseup', function(){
-	$('#slides').css({
-		display: 'none'
-	});
-	$(this).html('Show intro');
+$('#toggle_slides a').bind('mouseup', function(){
+	if($('#slides').css('display') != 'none'){
+		$('#slides').css({
+			display: 'none'
+		});
+		$('#navButtons').css({
+			display: 'none'
+		});	
+		$(this).html('Show intro');
+	}else{
+		$('#slides').css({
+			display: 'block'
+		});
+		$('#navButtons').css({
+			display: 'block'
+		});	
+		$(this).html('Hide intro');		
+	}
+
 });
